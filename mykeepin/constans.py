f4d99985174559eb5e27c40366976a6bf090a1dc
inyong_lee <inyong@coinplug.com>
2020-03-27 18:24:32 +0900

    Init

---
+++ mykeepin/constans.py
@@ -0,0 +1,5 @@
+MAINNET_RESOLVER_URL = "https://resolver.metadium.com/1.0/"
+TESTNET_RESOLVER_URL = "https://testnetresolver.metadium.com/1.0/"
+
+MAINNET_AUTH_SRV_URL = "https://auth.metadium.com/"
+TESTNET_AUTH_SRV_URL = "https://testauth.metadium.com/"
