d60871a9adc83f6621d891a1e1cbf0f58f244afb
inyong_lee <inyong@coinplug.com>
2020-10-16 18:26:50 +0900

    dependency package 대응

--- setup.py
+++ setup.py
@@ -6,29 +6,17 @@ with open("README.md", "r") as fh:
 
 loc = path.abspath(path.dirname(__file__))
 
-with open(loc + '/requirements.txt') as f:
-    requirements = f.read().splitlines()
-
-required = []
+required = [
+    "web3",
+    "ecdsa",
+    "requests",
+    "jwcrypto"
+]
 dependency_links = []
-# do not add to required lines pointing to git repositories
-EGG_MARK = '#egg='
-for line in requirements:
-    if line.startswith('-e git:') or line.startswith('-e git+') or \
-            line.startswith('git:') or line.startswith('git+'):
-        if EGG_MARK in line:
-            package_name = line[line.find(EGG_MARK) + len(EGG_MARK):]
-            required.append(package_name)
-            dependency_links.append(line)
-        else:
-            print('Dependency to a git repository should have the format:')
-            print('git+ssh://git@github.com/xxxxx/xxxxxx#egg=package_name')
-    else:
-        required.append(line)
 
 setuptools.setup(
     name="mykeepin-sdk",
-    version="0.2.dev1",
+    version="0.2.0",
     license="LGPLv3+",
     maintainer="Coinplug, Inc.",
     maintainer_email="inyong@coinplug.com",
